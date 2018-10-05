09ef62457bbd79a588ac19dc828c4cf02912ee97
komathisekar <komathis210@gmail.com>
2018-10-04 23:15:57 -0700

    second commit

--- pythonexcel2.py
+++ pythonexcel2.py
@@ -14,12 +14,11 @@ try:
   k=2
  for i in range(sheet.nrows):
   print(sheet.cell_value(i,k))
+ from xlwt import Workbook
+ wbs=Workbook()
+ sheet1=wbs.add_sheet('sheet 1')
+ for i in range(sheet.nrows):
+  sheet1.write(i,0,sheet.cell_value(i,k))
+ wbs.save('excels.xls')
 except NameError:
  print("give proper input")
-from xlwt import Workbook
-wbs=Workbook()
-sheet1=wbs.add_sheet('sheet 1')
-for i in range(sheet.nrows):
- sheet1.write(i,0,sheet.cell_value(i,k))
-wbs.save('excels.xls')
-
