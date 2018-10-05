9ddb4a1ed0cd5036a08bb15a5da7f69121d983ca
komathisekar <komathis210@gmail.com>
2018-10-04 21:28:59 -0700

    third commit

--- pythonexcel1.py
+++ pythonexcel1.py
@@ -7,7 +7,7 @@ wb=xlrd.open_workbook(loc)
 sheet=wb.sheet_by_index(0)
 sheet.cell_value(0,0)
 value=[]
-team_name=input("Enter the team name\n1.ODH\n2.EYE-BALL\n3.DEVOPS\n4.NA\n.others\n")
+team_name=input("Enter the team name\n1.ODH\n2.EYE-BALL\n3.DEVOPS\n4.NA\n5.others\n")
 try:
  if team_name == "ODH":
   for col in range(sheet.nrows):
@@ -37,6 +37,15 @@ try:
     value.append(sheet.cell_value(col,0))
     value.append(sheet.cell_value(col,1))
   print(value)
-except:
+ elif team_name == "others":
+  for col in range(sheet.nrows):
+   if sheet.cell_value(col,2) == "others":
+    k=col
+    value.append(sheet.cell_value(col,0))
+    value.append(sheet.cell_value(col,1))
+  print(value)
+ else:
+  print(value1)
+except NameError:
  print("give correct team name")
 
