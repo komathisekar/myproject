8b98c23727089c7d3277db9bcda7d03fa9d20c22
komathisekar <komathis210@gmail.com>
2019-07-16 12:54:48 +0530

    Adding the comments for Assessment - 1

--- wraptest_script.py
+++ wraptest_script.py
@@ -1,14 +1,15 @@
-import textwrap3
+#Program which  wrap the string into a paragraph of width .
+import textwrap3  #textwrap module can be used for wrapping and formatting of plain text
 String = input("Enter a String : ")  #string input
 count=len(String)     #finding the string length
-try:
+try:        # error handling if the provided width is not an integer
  width =int(input("Enter the width : "))  
  if(width > count):    #comparing the width & string length
   print("Entered width is greater than String count")
  else:
-  wrapper = textwrap3.TextWrapper(width) 
-  word_list = wrapper.wrap(text=String)
+  wrapper = textwrap3.TextWrapper(width)  #function wraps the input paragraph such that 
+  word_list = wrapper.wrap(text=String)   #each line in the paragraph is at most width characters long, returns output as list
   for element in word_list: 
-   print(element)
+   print(element)  # printing the list
 except ValueError:
  print("Enter a valid number")
